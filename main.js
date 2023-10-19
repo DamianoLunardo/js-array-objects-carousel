faf3e3d775563131e4cdeb88d028a4acc73f2b42
DamianoL <damianolunardo@hotmail.it>
2023-10-19 16:00:41 +0200

    ADD milestone 1 and 2

--- main.js
+++ main.js
@@ -25,3 +25,50 @@ const images = [
 		text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
 	},
 ]
+
+const carousel = document.getElementById('carousel');
+let currentSlide = 0;
+
+function createSlide(index) {
+	const slide = document.createElement('div');
+	slide.classList.add('carousel-slide');
+
+	const image = document.createElement('img');
+	image.src = images[index].image;
+
+	const textContainer = document.createElement('div');
+	textContainer.classList.add('carousel-text-container');
+
+	const title = document.createElement('h2');
+	title.textContent = images[index].title;
+
+	const text = document.createElement('p');
+	text.textContent = images[index].text;
+
+	textContainer.appendChild(title);
+	textContainer.appendChild(text);
+
+	slide.appendChild(image);
+	slide.appendChild(textContainer);
+
+	return slide;
+}
+
+function showSlide(index) {
+	carousel.innerHTML = '';
+	carousel.appendChild(createSlide(index));
+}
+
+function changeSlide(direction) {
+	currentSlide += direction;
+
+	if (currentSlide < 0) {
+		currentSlide = images.length - 1;
+	} else if (currentSlide >= images.length) {
+		currentSlide = 0;
+	}
+
+	showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
